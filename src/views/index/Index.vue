b35bda30f74e3f7ff5d159eab9ad420977180e52
lt00lt00lt <494858549@qq.com>
2019-11-19 20:42:05 +0800

    '初始化项目，创建新首页页面'

---
+++ src/views/index/Index.vue
@@ -0,0 +1,39 @@
+<template>
+  <page-view :title="title">
+    <div class="title">Hello World !</div>
+    <button @click="getData()">获取数据</button>
+  </page-view>
+</template>
+
+<script>
+import { PageView } from '@/layouts'
+import axios from 'axios'
+
+export default {
+  name: 'Index',
+  components: {
+    PageView
+  },
+  computed: {
+    title() {
+      return this.$route.meta.title
+    }
+  },
+  methods: {
+    getData() {
+      axios.post('/api/seniorFind/duty?pageNum=1&pageSize=3', {}).then(res=>{
+          console.log(res);
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.title {
+  color: rgba(0, 0, 0, 0.85);
+  font-size: 16px;
+  font-weight: 500;
+  margin-bottom: 16px;
+}
+</style>
